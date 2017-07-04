bf86ced0d80d9c3674237006c272d3bd9d424c0e
mariuszKrzysztofPrzybysz <mariusz.krzysztof.przybysz@gmail.com>
2017-07-04 21:49:54 +0200

    Add Model Classes

---
+++ BookService.Web/Models/Author.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookService.Web.Models
+{
+    public class Author
+    {
+        #region Persisted fields
+
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        #endregion
+    }
+}
---
+++ BookService.Web/Models/Book.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookService.Web.Models
+{
+    public class Book
+    {
+        #region Persisted fields
+
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public int Year { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Genre { get; set; }
+
+        public int AuthorId { get; set; }
+
+        #endregion
+
+        #region Navigation properties
+
+        public Author Author { get; set; }
+
+        #endregion
+    }
+}
